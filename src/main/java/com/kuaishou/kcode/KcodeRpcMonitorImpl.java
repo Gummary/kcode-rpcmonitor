4c1c312789b95f4e022ba296cd008011484c8ffc
ICT <ICTbjpGroup@outlook.com>
2020-06-27 10:47:17 +0800

    test

--- src/main/java/com/kuaishou/kcode/KcodeRpcMonitorImpl.java
+++ src/main/java/com/kuaishou/kcode/KcodeRpcMonitorImpl.java
@@ -257,7 +257,7 @@ public class KcodeRpcMonitorImpl implements KcodeRpcMonitor {
 
         range3CalledTime++;
         globalAverageMeter.updateTimer(RANGE3TIMER);
-        if(range3CalledTime > 15e4) {
+        if(range3CalledTime > 20e4) {
             globalAverageMeter.getStatistic();
         }
 
