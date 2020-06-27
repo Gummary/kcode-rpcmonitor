99a19085de621218da14ee373283a957c3b039a5
MTwz <mtwz@foxmail.com>
2020-06-27 08:57:20 +0800

    test

--- src/main/java/com/kuaishou/kcode/KcodeRpcMonitorImpl.java
+++ src/main/java/com/kuaishou/kcode/KcodeRpcMonitorImpl.java
@@ -257,7 +257,7 @@ public class KcodeRpcMonitorImpl implements KcodeRpcMonitor {
 
         range3CalledTime++;
         globalAverageMeter.updateTimer(RANGE3TIMER);
-        if(range3CalledTime > 3e4) {
+        if(range3CalledTime > 5e4) {
             globalAverageMeter.getStatistic();
         }
 
