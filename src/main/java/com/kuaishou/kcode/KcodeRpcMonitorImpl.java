85217cb34b5483f58f2a507727c4f2947b011cc7
Gummary <sdythp@gmail.com>
2020-06-29 15:05:46 +0800

    finish

--- src/main/java/com/kuaishou/kcode/KcodeRpcMonitorImpl.java
+++ src/main/java/com/kuaishou/kcode/KcodeRpcMonitorImpl.java
@@ -1,277 +1,141 @@
 package com.kuaishou.kcode;
 
-import com.kuaishou.kcode.handler.BuildRPCMessageHandler;
-import com.kuaishou.kcode.model.*;
-import com.kuaishou.kcode.utils.GlobalAverageMeter;
-
-import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.math.RoundingMode;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
-import java.text.DecimalFormat;
-import java.util.*;
-import java.util.Map.Entry;
-import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.io.*;
+import java.util.HashSet;
+import java.util.List;
 
 /**
- * @author kcode
- * Created on 2020-06-01
- * 实际提交时请维持包名和类名不变
+ * @team 辰海飞燕
+ * @member 陈元崇 - 济南大学泉城学院
+ * @member 张浩宇 - 哈尔滨工业大学
+ * @member 王俊杰 - 南京理工大学
+ *
+ * Created on 2020-06-17
+ *
+ *
  */
 
 public class KcodeRpcMonitorImpl implements KcodeRpcMonitor {
 
-    public static final long BLOCK_SIZE = Integer.MAX_VALUE;
-    private static final int CORE_THREAD_NUM = 7;
-    private static final ExecutorService rpcMessageHandlerPool = Executors.newFixedThreadPool(CORE_THREAD_NUM);//new ThreadPoolExecutor(CORE_THREAD_NUM, MAX_THREAD_NUM, TIME_OUT, TimeUnit.SECONDS, new SynchronousQueue<>());
-    public RandomAccessFile rpcDataFile;
-    public FileChannel rpcDataFileChannel;
-    private final ConcurrentHashMap<Integer, ConcurrentHashMap<String, ConcurrentHashMap<String, Range2Result>>> range2MessageMap = new ConcurrentHashMap<>(32);
-    private final ConcurrentHashMap<String, ConcurrentHashMap<Integer, SuccessRate>> range3Result;
-    private BuildRPCMessageHandler[] writeRPCMessageHandlers;
+    /* 答案 */
+    private Answer answer = new Answer();
 
-    private static DecimalFormat format;
+    /* 数据区域 */
+    private HashSet<String> methodAndIpPairStringSet = new HashSet<>();
+    private String[] methodAndIpPairStringArray = null;
 
-
-    //利用线程池优化2,3阶段
-    private static final ExecutorService range23ComputePool = Executors.newFixedThreadPool(CORE_THREAD_NUM);
-    private static final AtomicInteger computeIdx = new AtomicInteger();
-//    private static final ConcurrentHashMap<String, ArrayList<String>> computedRange2Result = new ConcurrentHashMap<>(500000);
-//    private static final ConcurrentHashMap<Integer, ConcurrentHashMap<String, ArrayList<String>>> computedRange2Result = new ConcurrentHashMap<>();
-    private static HashMap<String, ArrayList<String>>[] computedRange2Result=null;
-    private static int range2MintimeStamp;
-    private static int range2MaxTimeStamp;
-    private static final ConcurrentHashMap<String, Range3Result> computedRange3Result = new ConcurrentHashMap<>(512);
-    private static final StringBuilder range2KeyBuilder = new StringBuilder();
-
-    // Timer Setting
-    private static GlobalAverageMeter globalAverageMeter = new GlobalAverageMeter();
-    private static final String PREPARETIMER = "PREPARE";
-    private static final String RANGE2TIMER = "RANGE2";
-    private static final String RANGE3TIMER = "RANGE3";
-    private static int range3CalledTime = 0;
-
-    
-    //TEST
-    // 不要修改访问级别
+    /* 构造函数 */
     public KcodeRpcMonitorImpl() {
-        format = new DecimalFormat("#.00");
-        format.setRoundingMode(RoundingMode.DOWN);
-        range3Result = new ConcurrentHashMap<>();
 
-        globalAverageMeter.createTimer(PREPARETIMER);
-//        globalAverageMeter.createTimer(RANGE2TIMER);
-//        globalAverageMeter.createTimer(RANGE3TIMER);
     }
 
+    /**
+     * 线下数据集信息：
+     *  nr_caller   nr_ip_caller    nr_responder    nr_ip_responder     scope_time_minute
+     *  78          439             70              388                 30
+     */
+    public void prepare(String path) throws IOException {
+        /* 初始化当前时间 */
+        BufferedReader inputStream = new BufferedReader(new FileReader(path));
+        int currTime = (int) (Long.parseLong(inputStream.readLine().split(",")[6]) / (1000 * 60));
+        inputStream.close();
 
-    @Override
-    public void prepare(String path) throws Exception {
-        globalAverageMeter.startTimer(PREPARETIMER);
-        RandomAccessFile randomAccessFile = null;
-        try {
-            randomAccessFile = new RandomAccessFile(path, "r");
-            this.rpcDataFile = randomAccessFile;
-            this.rpcDataFileChannel = randomAccessFile.getChannel();
-            long fileSize = randomAccessFile.length();
-            //下取整
-            int maxBlockSize = (int) (fileSize / BLOCK_SIZE);
-            //存在剩余 -> block数 + 1
-            maxBlockSize = fileSize % BLOCK_SIZE == 0 ? maxBlockSize : maxBlockSize + 1;
+        /* 得到所有主调对 */
+        inputStream = new BufferedReader(new FileReader(path));
+        String line;
+        String[] split;
 
-            writeRPCMessageHandlers = new BuildRPCMessageHandler[maxBlockSize];
+        String methodAndIpPairString;
+        String caller;      /* 调用方 */
+        String callerIP;    /* 调用方 IP */
+        String responder;      /* 被调用目标方 */
+        String responderIP;    /* 被调用目标方 IP */
+        boolean success;      /* 调用成功？ */
+        int elapsedTime;    /* 调用耗时 */
+        int startTime;      /* 调用开始时间 */
+        int methodAndIpPairSize;
+        boolean findOut = false;
+        while ( !findOut ) {
+            line = inputStream.readLine();
+            /* 先转换 */
+            split = line.split(",");
+            caller = split[0];
+            callerIP = split[1];
+            responder = split[2];
+            responderIP = split[3];
+            success = Boolean.parseBoolean(split[4]);
+            elapsedTime = Integer.parseInt(split[5]);
+            startTime = (int) (Long.parseLong(split[6]) / (1000 * 60));   /* 按分钟算 */
+            methodAndIpPairString = caller + responder + "|" + callerIP + "," + responderIP;
 
-            CountDownLatch latch = new CountDownLatch(maxBlockSize);
+            if(startTime != currTime) {
+                /* 将所有主被调对转换为数组 */
+                methodAndIpPairSize = methodAndIpPairStringSet.size();
+                methodAndIpPairStringArray
+                        = methodAndIpPairStringSet.toArray(new String[methodAndIpPairSize]);
 
-            StringBuilder remindBufferBuilder = new StringBuilder();
-            // 分块读取文件
-            for (int currentBlock = 0; currentBlock < maxBlockSize; currentBlock++) {
-
-                writeRPCMessageHandlers[currentBlock] = new BuildRPCMessageHandler(range2MessageMap, range3Result);
-
-                int mapSize;
-                mapSize = (int) ((currentBlock == maxBlockSize - 1) ? (fileSize - (maxBlockSize - 1) * BLOCK_SIZE) : BLOCK_SIZE);
-                MappedByteBuffer mappedByteBuffer = rpcDataFileChannel.map(FileChannel.MapMode.READ_ONLY, currentBlock * BLOCK_SIZE, mapSize);
-
-                int lastLR = mapSize - 1;
-                while (mappedByteBuffer.get(lastLR) != '\n') {
-                    lastLR -= 1;
+                /* 计算 */
+                for(String mp : methodAndIpPairStringArray) {
+                    answer.pairAnswerCompute(mp, currTime);
                 }
 
-                BuildRPCMessageHandler rpcMessageHandler = writeRPCMessageHandlers[currentBlock];
-                if(currentBlock == 0) {
-                    rpcMessageHandler.setNewByteBuff(mappedByteBuffer, "", 0, lastLR, latch);
-                }else {
-                    rpcMessageHandler.setNewByteBuff(mappedByteBuffer, remindBufferBuilder.toString(), 0, lastLR, latch);
-                }
-
-                remindBufferBuilder.setLength(0);
-                for (int i = lastLR+1; i < mapSize; i++) {
-                    remindBufferBuilder.append((char)mappedByteBuffer.get(i));
-                }
-
-                rpcMessageHandlerPool.execute(rpcMessageHandler);
+                /* 更新当前时间 */
+                currTime = startTime;
+                methodAndIpPairStringSet.clear();
+                findOut = true;
             }
 
-            latch.await();
-            computedRange2Result = new HashMap[range2MessageMap.size()];
-            computeRange2Result();
-            computeRange3Result();
+            /* 将一个主被调对加入集合 */
 
-
-        } catch (InterruptedException | IOException ignored) {
-        } finally {
-            rpcMessageHandlerPool.shutdown();
-            range23ComputePool.shutdown();
-            globalAverageMeter.updateTimer(PREPARETIMER);
-            if (randomAccessFile != null) {
-            	try {
-					randomAccessFile.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
+            methodAndIpPairStringSet.add(methodAndIpPairString);
+            /* 加入到输入数据区域 */
+            answer.addPairData(methodAndIpPairString, elapsedTime, success);
+            answer.addResponderData(responder, startTime, success);
         }
-        String prepareStatistic = globalAverageMeter.getStatisticString();
-        String thread0Statistic = writeRPCMessageHandlers[0].threadAverageMeter.getStatisticString();
-        throw new Exception(String.format("%s %s", prepareStatistic, thread0Statistic));
-//        System.out.println(String.format("%s %s", prepareStatistic, thread0Statistic));
-    }
 
-    private void computeRange3Result() throws InterruptedException {
-        CountDownLatch latch = new CountDownLatch(CORE_THREAD_NUM);
-        String[] keyList = range3Result.keySet().toArray(new String[0]);
-        computeIdx.set(0);
-        for (int i = 0; i < CORE_THREAD_NUM; i++) {
-            range23ComputePool.execute(() -> {
-                int workIndex = computeIdx.getAndIncrement();
+        /* 开始读取并计算 */
+        while ( (line = inputStream.readLine()) != null ) {
+            /* 先转换 */
+            split = line.split(",");
+            caller = split[0];
+            callerIP = split[1];
+            responder = split[2];
+            responderIP = split[3];
+            success = Boolean.parseBoolean(split[4]);
+            elapsedTime = Integer.parseInt(split[5]);
+            startTime = (int) (Long.parseLong(split[6]) / (1000 * 60));   /* 按分钟算 */
+            methodAndIpPairString = caller + responder + "|" + callerIP + "," + responderIP;
 
-                while (workIndex < keyList.length) {
-                    String workKey = keyList[workIndex];
-
-                    ConcurrentHashMap<Integer, SuccessRate> minuteSuccessRate = range3Result.get(workKey);
-                    Range3Result range3Result = new Range3Result();
-                    for (Entry<Integer, SuccessRate> entry :
-                            minuteSuccessRate.entrySet()) {
-                        int minuteTimeStamp = entry.getKey();
-                        SuccessRate successRate = entry.getValue();
-                        double rate = (double) successRate.success.get() / successRate.total.get();
-                        range3Result.addTimeStampSuccessate(minuteTimeStamp, rate);
-                    }
-                    range3Result.calculatePrefixSum();
-                    computedRange3Result.put(workKey, range3Result);
-                    workIndex = computeIdx.getAndIncrement();
+            if(startTime != currTime) {
+                /* 计算 */
+                for(String mp : methodAndIpPairStringArray) {
+                    answer.pairAnswerCompute(mp, currTime);
                 }
-                latch.countDown();
-            });
+
+                /* 更新当前时间 */
+                currTime = startTime;
+            }
+            /* 加入到输入数据区域 */
+            answer.addPairData(methodAndIpPairString, elapsedTime, success);
+            answer.addResponderData(responder, startTime, success);
         }
-        latch.await();
+        for(String mp : methodAndIpPairStringArray) {
+            answer.pairAnswerCompute(mp, currTime);
+        }
     }
 
-    private void computeRange2Result() throws InterruptedException {
 
-        CountDownLatch latch = new CountDownLatch(CORE_THREAD_NUM);
-        Integer[] keyList = range2MessageMap.keySet().toArray(new Integer[0]);
-        range2MintimeStamp = Collections.min(range2MessageMap.keySet());
-        range2MaxTimeStamp = Collections.max(range2MessageMap.keySet());
-        computeIdx.set(0);
-        for (int i = 0; i < CORE_THREAD_NUM; i++) {
-            range23ComputePool.execute(() -> {
-                int workIndex = computeIdx.getAndIncrement();
-                StringBuilder builder = new StringBuilder();
-                while (workIndex < keyList.length) {
-                    int workMinuteStamp = keyList[workIndex];
-                    ConcurrentHashMap<String, ConcurrentHashMap<String, Range2Result>> functionMap = range2MessageMap.get(workMinuteStamp);
-                    int index= workMinuteStamp - range2MintimeStamp;
-                    computedRange2Result[index] = new HashMap<>();
-                    HashMap<String, ArrayList<String>> timestampMap = computedRange2Result[index];
-                    for (Entry<String, ConcurrentHashMap<String, Range2Result>> node : functionMap.entrySet()) {
-                        String key = node.getKey();
-                        ConcurrentHashMap<String, Range2Result> valueMap = node.getValue();
-                        Iterator<Entry<String, Range2Result>> resultIterator = valueMap.entrySet().iterator();
-                        ArrayList<String> resultList = new ArrayList<>();
-                        while (resultIterator.hasNext()) {
-                            Range2Result resultNode = resultIterator.next().getValue();
-                            builder.setLength(0);
-                            builder.append(resultNode.mainIP).append(",")
-                                    .append(resultNode.calledIP).append(",")
-                                    .append(resultNode.computeSuccessRate(format)).append(",")
-                                    .append(resultNode.computeP99());
-                            resultList.add(builder.toString());
-                        }
-//                        String date = DateUtils.minuteTimeStampToDate(workMinuteStamp);
-                        timestampMap.put(key, resultList);
-//                        computedRange2Result.put(key + date, resultList);
-                    }
-                    workIndex = computeIdx.getAndIncrement();
-                }
-                latch.countDown();
-            });
-        }
-        latch.await();
-    }
 
     @Override
     public List<String> checkPair(String caller, String responder, String time) {
-
-//        if (!globalAverageMeter.isTimerStarted(RANGE2TIMER)) {
-//            globalAverageMeter.startTimer(RANGE2TIMER);
-//            globalAverageMeter.updateTimerStart(RANGE2TIMER);
-//        }
-
-        range2KeyBuilder.setLength(0);
-        int timeKey = DateUtils.DateToMinuteTimeStamp(time);
-        if(timeKey < range2MintimeStamp || timeKey > range2MaxTimeStamp) {
-            return new ArrayList<>();
-        }
-        String range2Key = range2KeyBuilder.append(caller).append(responder).toString();
-
-        Map<String, ArrayList<String>> service2Result = computedRange2Result[timeKey-range2MintimeStamp];
-        ArrayList<String> result = service2Result.get(range2Key);
-
-        return result == null ? new ArrayList<>() : result;
+        return answer.getPairAnswer(caller, responder, time);
     }
+
 
 
     @Override
-    public String checkResponder(String responder, String start, String end) throws Exception {
-
-//        if (!globalAverageMeter.isTimerStarted(RANGE3TIMER)) {
-//        	globalAverageMeter.updateTimer(RANGE2TIMER);
-//            globalAverageMeter.startTimer(RANGE3TIMER);
-//            range3CalledTime = 0;
-//        }
-
-        Range3Result range3Result = computedRange3Result.get(responder);
-        String resultString = ".00%";
-        if (range3Result == null) {
-            resultString = "-1.00%";
-        } else {
-            double resultDouble = range3Result.getResult(DateUtils.DateToMinuteTimeStamp(start), DateUtils.DateToMinuteTimeStamp(end));
-            resultDouble *= 100;
-            if (resultDouble - 0.0d > 1e-4) {
-                resultString = format.format(resultDouble) + "%";
-            }
-        }
-
-//        range3CalledTime++;
-//
-//        if(range3CalledTime >= 3e5) {
-//        	globalAverageMeter.updateTimer(RANGE3TIMER);
-//            globalAverageMeter.getStatistic();
-//        }
-
-        return resultString;
+    public String checkResponder(String responder, String start, String end) {
+        return answer.getResponderAnswer(responder, start, end);
     }
 
-
-    @Deprecated
-    public void writeMinuteRPCMEssgaeToFile(int Minute) {
-//    	ConcurrentHashMap<String, ConcurrentLinkedQueue<FileRPCMessage>> minuteMap = range2MessageMap.get(Minute);
-//    	if(minuteMap == null) {
-//    		new WriteMessageToFileThread(writeToFileHandlerPool, minuteMap, files, Minute).start();
-//    	}
-    }
 }
